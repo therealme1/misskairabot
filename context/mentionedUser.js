380e8ddad9f4ca8073bb9b6cade30af5d5b63b5c
TheDevXen <thedevxen@gmail.com>
2019-06-27 20:05:04 +0530

    added mentioned middleware

---
+++ context/mentionedUser.js
@@ -0,0 +1,24 @@
+const db = require('../db');
+
+module.exports = bot => {
+    bot.context.getMentioned = async function() {
+        const { message } = this;
+        const { reply_to_message: reply } = this.message;
+        const [entity] = (this.message.entities || []).filter(
+            entity => entity.type === 'mention'
+        );
+
+        if (reply !== undefined) {
+            return reply.from;
+        } else if (entity !== undefined) {
+            const _user = message.text.substring(
+                entity.offset + 1,
+                entity.offset + entity.length
+            );
+            const user = await db('users').where({ username: _user });
+            return user[0];
+        } else {
+            return { user_id: null };
+        }
+    };
+};
