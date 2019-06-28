c9cf6970f1a80c5127e7cbc8be7570f3e8521e41
TheDevXen <thedevxen@gmail.com>
2019-06-28 20:31:16 +0530

    add plugin system

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const middlewares = ['admin', 'addUserToDb'];
 const contexts = ['lang', 'mentionedUser'];
+const plugins = ['ping']; // default plugins
 
 contexts.forEach(ctx => {
     require(`./context/${ctx}`)(bot);
 });
 middlewares.forEach(mid => {
     bot.on('message', require(`./middleware/${mid}`));
+});
+plugins.forEach(plugin => {
+    require(`./plguins/default/${plugin}`)(bot);
 });
 
 require('./handlers')(bot);
