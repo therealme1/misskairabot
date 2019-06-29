97b0f30cb5c9e29bcdb3d32f581e0187cf7a8ecc
TheDevXen <thedevxen@gmail.com>
2019-06-29 13:14:41 +0530

    fix

--- plguins/default/eval/index.js
+++ plguins/default/eval/index.js
@@ -6,7 +6,7 @@ module.exports = bot => {
     bot.command('eval', async ctx => {
         const command = ctx.args().join(' ');
         const bot_admin = process.env.BOT_ADMIN.split(',');
-        if (bot_admin.includes(tx.from.id)) {
+        if (bot_admin.includes(ctx.from.id)) {
             const res = eval(command);
             console.log(res);
             ctx.reply(String(res));
