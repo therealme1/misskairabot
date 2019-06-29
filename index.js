d2336fd326df269824bb4fbdd755eb861da75693
TheDevXen <thedevxen@gmail.com>
2019-06-29 09:37:05 +0530

    added reply context

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Telegraf = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const middlewares = ['checkForAdmin', 'addUserToDb'];
-const contexts = ['args', 'lang', 'admin', 'mentionedUser'];
+const contexts = ['args', 'lang', 'reply', 'admin', 'mentionedUser'];
 const plugins = ['ping', 'locks', 'urban-dictionary']; // default plugins
 
 contexts.forEach(ctx => {
