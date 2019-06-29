ded21ec6887d151bb0445160dca0a544cd793659
TheDevXen <thedevxen@gmail.com>
2019-06-29 13:07:50 +0530

    added eval

---
+++ plguins/default/eval/index.js
@@ -0,0 +1,15 @@
+process.exit = () => 'No!';
+process.kill = () => 'No!';
+process.abort = () => 'No!';
+
+module.exports = bot => {
+    bot.command('eval', async ctx => {
+        const command = ctx.args().join(' ');
+        const bot_admin = process.env.BOT_ADMIN.split(',');
+        if (bot_admin.includes(tx.from.id)) {
+            const res = eval(command);
+            console.log(res);
+            ctx.reply(String(res));
+        }
+    });
+};
