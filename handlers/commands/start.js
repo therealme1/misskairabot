ee58038dbe74c35bcee2ce9ee6ec95c15b32a2a2
TheDevXen <thedevxen@gmail.com>
2019-06-29 10:58:26 +0530

    added conditions for gban

--- handlers/commands/start.js
+++ handlers/commands/start.js
@@ -1,15 +1,39 @@
 const escapeHtml = require('@youtwitface/escape-html');
 const i18n = require('../../utils/i18n');
 
+const CONDITIONS_FOR_SUPER_USER = [
+    'You must be a contributor',
+    'You must be in more than 10 telegram groups',
+    'You must understand English language',
+    'You must be active on telegram'
+];
+
 module.exports = bot => {
     bot.start(async ctx => {
-        ctx.reply(
-            i18n(
-                await ctx.lang(),
-                `start.${ctx.chat.type === 'private' ? 'private' : 'group'}`,
-                { name: escapeHtml(ctx.from.first_name) }
-            ),
-            { parse_mode: 'html' }
-        );
+        if (ctx.args()) {
+            let param = ctx.args()[0];
+            switch (param) {
+                case 'help_superuser':
+                    let conditions = '';
+                    CONDITIONS_FOR_SUPER_USER.forEach((c, index) => {
+                        conditions += `${index + 1} ${c}\n`;
+                    });
+                    ctx.reply(
+                        `Following are the conditions to become super user for ${
+                            ctx.botInfo.username
+                        }:\n${conditions}`
+                    );
+            }
+        } else
+            ctx.reply(
+                i18n(
+                    await ctx.lang(),
+                    `start.${
+                        ctx.chat.type === 'private' ? 'private' : 'group'
+                    }`,
+                    { name: escapeHtml(ctx.from.first_name) }
+                ),
+                { parse_mode: 'html' }
+            );
     });
 };
