3f8f35632190488de1184e8b5ae1f7b9785e2eff
TheDevXen <thedevxen@gmail.com>
2019-06-29 09:32:54 +0530

    added pagination to ud

--- plguins/default/urban-dictionary/index.js
+++ plguins/default/urban-dictionary/index.js
@@ -1,17 +1,76 @@
 const ud = require('urban-dictionary');
 const Filter = require('bad-words');
 const filter = new Filter();
+const { Markup, Extra } = require('telegraf');
 
 module.exports = bot => {
     bot.hears(/^\? (.*)/, async ctx => {
         const [, query] = ctx.match;
         ud.term(query, (error, entries, tags, sounds) => {
             if (error) {
-                return ctx.reply('No defination found!');
+                return ctx.reply('No definition found!');
             } else {
-                const random = Math.floor(Math.random() * entries.length);
-                const entry = entries[random];
-                ctx.reply(filter.clean(entry.definition));
+                const total = entries.length;
+                const entry = entries[0];
+                ctx.reply(
+                    filter.clean(entry.definition),
+                    Markup.inlineKeyboard([
+                        Markup.callbackButton(
+                            ' ⬅️',
+                            `urban_${Buffer.from(query).toString('base64')}_0`
+                        ),
+                        Markup.callbackButton(
+                            `1/${total}`,
+                            `urban_${Buffer.from(query).toString(
+                                'base64'
+                            )}_${total - 1}`
+                        ),
+                        Markup.callbackButton(
+                            ' ➡️',
+                            `urban_${Buffer.from(query).toString('base64')}_1`
+                        )
+                    ]).extra()
+                );
+            }
+        });
+    });
+
+    bot.action(/urban_(.*)_(\d+)/, async ctx => {
+        let [, query_, to_page] = ctx.match;
+        const query = Buffer.from(query_, 'base64').toString();
+        ud.term(query, async (error, entries, tags, sounds) => {
+            if (error) {
+                return ctx.reply('No definition found!');
+            } else {
+                const total = entries.length;
+                if (total - 1 < to_page) {
+                    to_page = total - 1;
+                }
+                const entry = entries[to_page];
+                await ctx.editMessageText(
+                    filter.clean(entry.definition),
+                    Markup.inlineKeyboard([
+                        Markup.callbackButton(
+                            ' ⬅️',
+                            `urban_${Buffer.from(query).toString(
+                                'base64'
+                            )}_${to_page - 1}`
+                        ),
+                        Markup.callbackButton(
+                            `${to_page}/${total - 1}`,
+                            `urban_${Buffer.from(query).toString(
+                                'base64'
+                            )}_${total - 1}`
+                        ),
+                        Markup.callbackButton(
+                            ' ➡️',
+                            `urban_${Buffer.from(query).toString(
+                                'base64'
+                            )}_${parseInt(to_page) + 1}`
+                        )
+                    ]).extra()
+                );
+                ctx.answerCbQuery();
             }
         });
     });
